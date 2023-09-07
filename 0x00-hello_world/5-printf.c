0fc76db71a6fc7fbe39d1e2368554274385f02ee
LinaAbdelkhalig <lelsammani0@gmail.com>
2023-09-07 15:18:31 -0700

    hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * main - the function should print a specfic test
+ * Return: the program should return 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
