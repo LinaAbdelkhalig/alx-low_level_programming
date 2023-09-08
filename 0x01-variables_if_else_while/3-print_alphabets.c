9f872ae66aea313a98e2fc290b802aaf722cf74d
LinaAbdelkhalig <lelsammani0@gmail.com>
2023-09-08 16:21:28 -0700

    alphaBET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - diply the alphabet
+ * Return: this will return 0
+*/
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	for (i = 'A'; i <= 'Z'; i++)
+		putchar(i);
+	putchar('\n');
+
+	return (0);
+}
