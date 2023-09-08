19557899754e7500fe43fe1ce564a529b28b659d
LinaAbdelkhalig <lelsammani0@gmail.com>
2023-09-08 16:10:27 -0700

    type the alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - diply the alphabet
+ * Return: this will return 0
+*/
+int main(void)
+{
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar("\n");
+
+	return (0);
+}
