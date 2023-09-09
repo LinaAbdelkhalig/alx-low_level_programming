433313a9dde643edd2fb6ac22fee8aa2d117c8d6
LinaAbdelkhalig <lelsammani0@gmail.com>
2023-09-08 17:45:26 -0700

    final fix

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,7 +8,7 @@ int main(void)
 	int i;
 	int j;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i <= 8; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
@@ -16,7 +16,7 @@ int main(void)
 			{
 				putchar(i + '0');
 				putchar(j + '0');
-				if (i != 8 && j != 9)
+				if (i + j != 89)
 				{
 					putchar(',');
 					putchar(' ');
