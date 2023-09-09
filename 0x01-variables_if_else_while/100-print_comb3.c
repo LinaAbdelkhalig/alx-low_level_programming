e6ebb1b15b8722edc0df4db90517e54471f0bc64
LinaAbdelkhalig <lelsammani0@gmail.com>
2023-09-08 17:33:03 -0700

    output

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,14 +10,13 @@ int main(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j < 9; j++)
+		for (j = 0; j <= 9; j++)
 		{
 			if (i != j && i < j)
 			{
 				putchar(i + '0');
 				putchar(j + '0');
 				putchar(',');
-				putchar(' ');
 			}
 		}
 	}
