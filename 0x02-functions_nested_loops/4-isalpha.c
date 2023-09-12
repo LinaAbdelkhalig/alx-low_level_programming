ae650e189c2cb43dee709004977420633d18cbd4
LinaAbdelkhalig <lelsammani0@gmail.com>
2023-09-12 11:57:25 -0700

    you always forget to take out the semicolon

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,9 +4,9 @@
  * @c: psram for checking
  * Return: 0 or 1
  */
-int _isalpha(int c);
+int _isalpha(int c)
 {
-	if((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
 	else
 		return (0);
