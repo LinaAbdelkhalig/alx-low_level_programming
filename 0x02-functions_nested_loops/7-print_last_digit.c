cb299cbc32617f1c8a3012f2b2335cd3d6f5b9c9
LinaAbdelkhalig <lelsammani0@gmail.com>
2023-09-12 12:14:45 -0700

    no

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_last_digit - print the last digit of a numver
+ * @i: will print the last digit of this
+ * Return: return the last difit
+ */
+int print_last_digit(int i)
+{
+	return (i%10);
+}
