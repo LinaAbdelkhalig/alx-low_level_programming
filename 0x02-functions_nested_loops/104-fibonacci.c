267815dbd4f21227841be91fd43a5e03a3627858
LinaAbdelkhalig <lelsammani0@gmail.com>
2023-09-12 14:20:05 -0700

    not gn

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -13,9 +13,9 @@ int main(void)
 	{
 		if (f1o > 0)
 			printf("%lu", f1o);
-		intial0s = numLength(mx) - 1 - numLength(f1);
+		initial0s = numLength(mx) - 1 - numLength(f1);
 
-		while (f1o > 0 && intial0s > 0)
+		while (f1o > 0 && initial0s > 0)
 		{
 			printf("%d", 0);
 			initial0s--;
