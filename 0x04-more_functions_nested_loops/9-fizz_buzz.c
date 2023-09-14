2d305906b4b972ffa23189c996b00868231d1ddd
LinaAbdelkhalig <lelsammani0@gmail.com>
2023-09-14 16:53:20 -0700

    fizzbuzz fiz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,20 +9,20 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && !(i % 5 == 0))
+		if (i % 3 == 0)
 			printf("Fizz ");
-		else if (i % 5 == 0 && !(i % 3 == 0))
+		else if (i % 5 == 0)
 			printf("Buzz ");
 		else if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz ");
 		else
-			printf("%d ", i);
+			printf("%d", i);
 
 		if (i == 100)
-			printf(" ");
-		else
-			printf("\n");
+			continue;
+		printf(" ");
 	}
+	printf("\n");
 
 	return (0);
 }
