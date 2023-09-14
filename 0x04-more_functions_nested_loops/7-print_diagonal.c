309ad25b189092d24d661c64e18623f04e9794c9
LinaAbdelkhalig <lelsammani0@gmail.com>
2023-09-14 16:08:10 -0700

    print a slash

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_diagonal - will print a diagonal line
+ * @n: the number of times the characters \ will be printed
+ */
+void print_diagonal(int n)
+{
+	int i, space;
+
+	if (n <= 0)
+		_putchar('\n');
+
+	for (i = 1; i <= n; i++)
+	{
+		for (space = 1; space <= i; space++)
+			_putchar(' ');
+		_putchar(92);
+		_putchar('\n');
+	}
+}
