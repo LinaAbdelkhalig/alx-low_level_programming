1fe3f4eea377d9df5991641886a458588cd0223b
LinaAbdelkhalig <lelsammani0@gmail.com>
2023-09-14 15:42:16 -0700

    ten lines of numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * more_numbers - will print 10 lines of numbers
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int row, count, num;
+
+	for (row = 0; row < 10; row++)
+	{
+		for (count = 0; count <= 9; count++)
+		{
+			num = count;
+			if (count > 9)
+			{
+				_putchar(1 + '0');
+				num = count % 10;
+			}
+			_putchar(num + '0');
+		}
+		_putchar('\n');
+	}
+}
