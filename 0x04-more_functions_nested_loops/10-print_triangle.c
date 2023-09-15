c06f380759fbc4f26a2803c3a000bbf3a9868f51
LinaAbdelkhalig <lelsammani0@gmail.com>
2023-09-14 17:12:53 -0700

    triangle fix

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -12,15 +12,18 @@ void print_triangle(int size)
 	{
 		for (hash = 1; hash <= size; hash++)
 		{
-			for (index = size - hash; index > 0; index --)
+			for (index = size - hash; index > 0; index--)
 				_putchar(' ');
 
-			for (index = 0; index < hash; index ++)
+			for (index = 0; index < hash; index++)
 				_putchar('#');
 
 			if (hash == size)
 				continue;
+			
 			_putchar('\n');	
 		}
 	}
+
+	_putchar('\n');
 }
