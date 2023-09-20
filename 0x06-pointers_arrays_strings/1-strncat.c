e510d8ef153233f612b953d84138078587cec604
LinaAbdelkhalig <lelsammani0@gmail.com>
2023-09-20 09:53:20 -0700

    FIX strncat ISO C90 forbids mixed declarations and code

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,12 +10,12 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int dest_len = 0;
+	int dest_len, i;
+
+	dest_len = 0;
 
 	while (dest[dest_len])
 		dest_len++;
-
-	int i;
 
 	for (i = 0 ; i < n && src[i] != '\0' ; i++)
 		dest[dest_len + i] = src[i];
