937bdb445e4e3a9a8c19eeb8f4e41d90dbdee684
LinaAbdelkhalig <lelsammani0@gmail.com>
2023-09-20 10:23:28 -0700

    stcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: the first string
+ * @s2: the second string
+ * Return: an int that is =, < , > 0
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int val = 0;
+
+	while (*s1)
+	{
+		if (*s1 != *s2)
+		{
+			val = ((int)*s1 - 48) - ((int)*s2 - 48);
+			break;
+		}
+		*s1++;
+		*s2++;
+	}
+
+	return (val);
+}
