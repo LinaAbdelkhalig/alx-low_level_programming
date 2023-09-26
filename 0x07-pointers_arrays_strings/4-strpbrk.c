28c591918eec961521282c27a6d0ad1c690541a1
LinaAbdelkhalig <lelsammani0@gmail.com>
2023-09-25 17:54:09 -0700

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strpbrk - locates the first occurence of a char from sccept in s
+ * @s: the string to be searched
+ * @accept: the bytes wwe will be searching for in s
+ * Return: pointer to the byte in s that is a match or NULL
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		while (s[i] != accept[j])
+		{
+			j++;
+			if (accept[j] == '\0')
+				return (s + i);
+		}
+	}
+	return (s + i);
+}
