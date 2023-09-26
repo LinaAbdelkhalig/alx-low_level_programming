2c2ce733d0f8c5c2d883f9869aa9a42e6621e065
LinaAbdelkhalig <lelsammani0@gmail.com>
2023-09-25 17:10:25 -0700

    strchr FIXED USED S + I INSTEAD OF POINTER

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,16 +10,14 @@
 char *_strchr(char *s, char c)
 {
 	int i;
-	char *ptr;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
 		{
-			ptr = &s[i];
-			return (ptr);
+			return (s + i);
 		}
 	}
 
-	return ('\0');
+	return (NULL);
 }
