c3fb377d6726a17db02f0418625efaedf22ee6fe
LinaAbdelkhalig <lelsammani0@gmail.com>
2023-09-25 18:53:03 -0700

    print diagsums fix conditions

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -12,10 +12,10 @@ void print_diagsums(int *a, int size)
 	int s1 = 0, s2 = 0, i;
 
 	for (i = 0; i < size; i++)
-		s1 += a[i];
+		s1 += a[i * size + i];
 
-	for (i = size - 1; i >= 0; i++)
-		s2 += a[i];
+	for (i = size - 1; i >= 0; i--)
+		s2 += a[i * size + (size - i - 1)];
 
 	printf("%d, %d\n", s1, s2);
 }
