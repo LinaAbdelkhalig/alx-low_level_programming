31d650d0ca35fb407751e79009abd8c1f6f4509c
LinaAbdelkhalig <lelsammani0@gmail.com>
2023-10-02 11:40:39 -0700

    it aint what they call you, its what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - program that prints its name followed by a new lline
+ * @argc: number of strings in the argv array
+ * @argv: this is the array containing the arguments for this function
+ * Return: 0
+ */
+
+int main(__attribute__((unused))int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+this is the directory for the argc, argv project
