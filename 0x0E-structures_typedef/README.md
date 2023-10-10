6dcb108df08192cb183e0cecdb94111e9e656593
LinaAbdelkhalig <lelsammani0@gmail.com>
2023-10-10 14:12:55 -0700

    poppy

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+this is the directory for the structs and typedef tasks
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog structure
+ * @name: name of the dog
+ * @age: age of the dog
+ * Owner: owner of the dog
+ *
+ * Description: woof
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
