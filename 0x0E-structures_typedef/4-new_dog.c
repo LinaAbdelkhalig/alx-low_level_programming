bd098748bd0a2cb42e27026a5962e14b7d388726
LinaAbdelkhalig <lelsammani0@gmail.com>
2023-10-10 15:49:00 -0700

    task 4 fix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,39 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * length - return the length of a string
+ * @string: this string
+ * Return: the length
+ */
+
+int length(char *string)
+{
+	int len = 0;
+
+	while (*string++)
+		len++;
+	return (len);
+}
+
+/**
+ * copy - copies one string onto another
+ * @org: the string that will be copied
+ * @dest: onto here
+ * Return: the copied string
+ */
+
+char *copy(char *dest, char *org)
+{
+	int i;
+
+	for (i = 0; org[i]; i++)
+		dest[i] = org[i];
+	dest[i] = '\0';
+
+	return (dest);
+}
 
 /**
  * new_dog - creates a new object of type dog
@@ -11,14 +45,33 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *d = malloc(sizeof(dog_t));
-	char *namei = name;
-	char *owneri = owner;
+	dog_t *d;
+
+	if (name == NULL || age < 0 || owner == NULL)
+		return (NULL);
+
+	d = (dog_t *) malloc(sizeof(dog_t));
 
 	if (d == NULL)
 		return (NULL);
-	d->name = namei;
+
+	d->name = malloc(sizeof(char) * (length(name) + 1));
+	if (d->name == NULL)
+	{
+		free(d);
+		return (NULL);
+	}
+
+	d->owner = malloc(sizeof(char) * (length(owner) + 1));
+	if (d->owner == NULL)
+	{
+		free(d->name);
+		free(d);
+		return (NULL);
+	}
+
+	d->name = copy(d->name, name);
 	d->age = age;
-	d->owner = owneri;
+	d->owner = copy(d->owner, owner);
 	return (d);
 }
