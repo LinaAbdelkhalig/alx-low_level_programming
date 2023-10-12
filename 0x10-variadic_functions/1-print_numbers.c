bde033b68497f5e78f11baad7f5d179124e018bb
LinaAbdelkhalig <lelsammani0@gmail.com>
2023-10-12 12:08:02 -0700

    to be is the to be of variable fix condittions

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,11 +14,18 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list list;
 	unsigned int i;
 
+	if (n == 0)
+	{
+		printf("\n");
+		return;
+	}
+
 	va_start(list, n);
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d%s", va_arg(list, int), separator);
+		printf("%d%s", va_arg(list, int), separator ? separator : "");
 	}
 	printf("\n");
+	va_end(list);
 }
