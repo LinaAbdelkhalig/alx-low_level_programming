5df83a23667e24571c4edb45c6ec2702ad185db1
LinaAbdelkhalig <lelsammani0@gmail.com>
2023-10-19 12:16:50 -0700

    free te list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a linked list
+ * @head: the head of the list
+ */
+
+void free_list(list_t *head)
+{
+	list_t *current, next;
+
+	if (!head)
+		return;
+	current = head;
+
+	while (current)
+	{
+		next = current->next;
+		free(current->str);
+		free(current);
+		current = next;
+	}
+}
