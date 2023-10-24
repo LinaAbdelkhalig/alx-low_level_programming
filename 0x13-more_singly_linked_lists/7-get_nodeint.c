2a87a17ff279cf4a3d308d9c0ff0aebdf5439575
LinaAbdelkhalig <lelsammani0@gmail.com>
2023-10-24 12:25:47 -0700

    find the node

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - returns the nth node of a listint
+ * @head: the head of the list
+ * @index: the index of the node;
+ * Return: the node if found or NULL
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *node;
+	unsigned int i;
+
+	for (node = head, n = 0; node && i < index; node = node->next, i++)
+		;
+	return (node);
+}
