32b1c4044a13fd781f4112b4ba4bca10e0521262
LinaAbdelkhalig <lelsammani0@gmail.com>
2023-10-24 12:28:00 -0700

    fixed

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -11,7 +11,7 @@ int pop_listint(listint_t **head)
 	listint_t *node;
 	int hn;
 
-	if (!head || *head)
+	if (!head || !*head)
 		return (0);
 
 	node = (*head)->next;
--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,7 +12,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *node;
 	unsigned int i;
 
-	for (node = head, n = 0; node && i < index; node = node->next, i++)
+	for (node = head, i = 0; node && i < index; node = node->next, i++)
 		;
 	return (node);
 }
