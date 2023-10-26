878175f1ff0c8ab1351f7e9e70e54177b031e322
LinaAbdelkhalig <lelsammani0@gmail.com>
2023-10-26 11:42:09 -0700

    fixed readme

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * power - finds the result of b to the power of p
  * @b: the base
@@ -6,16 +7,17 @@
  * Return: the result of b ^ p int
  */
 
-unsigned int power(int b, unsigned int p)
+int power(int b, int p)
 {
-	unsigned int res = 1, i;
+        int res = 1, i;
 
-	for (i = 0; i < p; i++)
-	{
-		res = res * b;
-	}
-	return (res);
+        for (i = 0; i < p; i++)
+        {
+                res = res * b;
+        }
+        return (res);
 }
+#include "main.h"
 
 /**
  * binary_to_uint - converts a binary to unsigned integer
@@ -25,20 +27,20 @@ unsigned int power(int b, unsigned int p)
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int pow = 0;
-	int unint = 0, binlen;
+        int binlen, pow = 0;
+        unsigned int unint = 0;
 
-	if (!b)
-		return (0);
-	for (binlen = 0; b[binlen] != '\0'; binlen++)
-	{
-		if (b[binlen] != '0' && b[binlen] != '1')
-			return (0);
-	}
-	for (binlen--; binlen >= 0; binlen--)
-	{
-		unint += (b[binlen] - '0') * power(2, pow);
-		pow++;
-	}
-	return (unint);
+        if (!b)
+                return (0);
+        for (binlen = 0; b[binlen] != '\0'; binlen++)
+        {
+                if (b[binlen] != '0' && b[binlen] != '1')
+                        return (0);
+        }
+        for (binlen-- ; binlen >= 0; binlen--)
+        {
+                unint += (b[binlen] - '0') * (unsigned int)power(2, pow);
+                pow++;
+        }
+        return (unint);
 }
---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,5 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary reperesentation of a number
+ * @n: 
