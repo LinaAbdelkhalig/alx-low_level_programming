e17fbeb46ac7b77436c043874d96913592ca47c0
LinaAbdelkhalig <lelsammani0@gmail.com>
2023-10-26 11:20:47 -0700

    fix

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -5,7 +5,6 @@
 #include <stdlib.h>
 
 int _putchar(char c);
-int power(int b, int p);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
