ab2eb00176ffaf3155539f5e0890611d9909d972
LinaAbdelkhalig <lelsammani0@gmail.com>
2023-10-30 13:20:14 -0700

    task 3 fix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 #define USE "Usage: cp file_from file_to\n"
-#define NORD "Error: Can't read from file NAME_OF_THE_FILE\n"
-#define NOWR "Error: Can't write to NAME_OF_THE_FILE\n"
-#define NOCLS "Error: Can't close fd FD_VALUE\n"
+#define NORD "Error: Can't read from file %s\n"
+#define NOWR "Error: Can't write to %s\n"
+#define NOCLS "Error: Can't close fd %d\n"
 #define PERM (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
 
 /**
@@ -17,28 +17,28 @@ int main(int argc, char **argv)
 {
 	int frm_fc = 0, to_fc = 0;
 	ssize_t size;
-	char buff[READ_BUFF_SIZE];
+	char buff[READ_BUF_SIZE];
 
 	if (argc != 3)
-		dprint(STDERR_FILENO, USE), exit(97);
+		dprintf(STDERR_FILENO, USE), exit(97);
 	frm_fc = open(argv[1],  O_RDONLY);
 	if (frm_fc == -1)
-		dprint(STDERR_FILENO, NORD, argv[1]), exit(98);
+		dprintf(STDERR_FILENO, NORD, argv[1]), exit(98);
 	to_fc = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, PERM);
 	if (to_fc == -1)
-		dprint(STDERR_FILENO, NOWR, argv[2]), exit(99);
+		dprintf(STDERR_FILENO, NOWR, argv[2]), exit(99);
 
 	while ((size = read(frm_fc, buff, READ_BUF_SIZE)) > 0)
 		if (write(to_fc, buff, size) != size)
-			dprint(STDERR_FILENO, NOWR, argv[2]), exit(99);
+			dprintf(STDERR_FILENO, NOWR, argv[2]), exit(99);
 	if (size == -1)
-		dprint(STDERR_FILENO, NORD, argv[1]), exit(98);
+		dprintf(STDERR_FILENO, NORD, argv[1]), exit(98);
 
 	frm_fc = close(frm_fc);
 	to_fc = close(to_fc);
 	if (frm_fc)
-		dprint(STDERR_FILENO, NOCLS, frm_fc), exit(100);
+		dprintf(STDERR_FILENO, NOCLS, frm_fc), exit(100);
 	if(to_fc)
-		dprint(STDERR_FILENO, NOCLS, frm_fc), exit(100);
+		dprintf(STDERR_FILENO, NOCLS, frm_fc), exit(100);
 	return (EXIT_SUCCESS);
 }
